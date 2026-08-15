1742dd9df0e549d005e7884a0cb54ece06788cdd
mehbobhq <onboard@truckeasesolutions.com>
2026-08-15 05:52:52 -0400

    Refactor imports and add Process component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from '@/components/home/hero'
-import { WhyTruckEase } from '@/components/home/why-truckease'
-import { PlatformPreview } from '@/components/home/platform-sections'
+import { PlatformPreview, WhyTruckEase, Process } from '@/components/home/platform-sections'
 import { Problem, HowWeHelp, Philosophy } from '@/components/home/value-sections'
 import { Testimonials } from '@/components/home/testimonials'
 import { RiskCta } from '@/components/home/risk-cta'
@@ -13,6 +12,7 @@ export default function HomePage() {
       <PlatformPreview />
       <Problem />
       <HowWeHelp />
+      <Process />
       <Philosophy />
       <Testimonials />
       <RiskCta />
