4fef4f95e38283e18600f3d42f751ec46be7b300
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 00:21:19 -0400

    Import StatsBanner component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { Hero } from '@/components/home/hero'
 import { DashboardPreview } from '@/components/home/dashboard-preview'
+import { StatsBanner } from '@/components/home/stats-banner'
 
 export default function Home() {
   return (
