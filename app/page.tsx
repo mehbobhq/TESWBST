105e388e9e4c57062740dc1c8f66af4f81007a0d
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 00:19:57 -0400

    Clean up Home component by removing unused imports
    Removed unused components from the Home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,9 @@
 import { Hero } from '@/components/home/hero'
-import { DashboardPreview } from '@/components/home/dashboard-preview'
-import { StatsBanner } from '@/components/home/stats-banner'
-import { WhyTruckEase } from '@/components/home/why-truckease'
-import { HowWeHelp } from '@/components/home/value-sections'
-import { ProcessSteps } from '@/components/home/process-steps'
-import { Testimonials } from '@/components/home/testimonials'
-import { RiskCta } from '@/components/home/risk-cta'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
       <Hero />
-      <DashboardPreview />
-      <StatsBanner />
-      <WhyTruckEase />
-      <HowWeHelp />
-      <ProcessSteps />
-      <Testimonials />
-      <RiskCta />
     </main>
   )
 }
