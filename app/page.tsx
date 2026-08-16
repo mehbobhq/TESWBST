078095dda823e1e6f2c4a425a9214d41d296c008
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 00:22:35 -0400

    Import HowWeHelp component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from '@/components/home/hero'
 import { DashboardPreview } from '@/components/home/dashboard-preview'
 import { StatsBanner } from '@/components/home/stats-banner'
 import { WhyTruckEase } from '@/components/home/why-truckease'
+import { HowWeHelp } from '@/components/home/value-sections'
 
 export default function Home() {
   return (
