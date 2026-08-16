216040066075d4075ef3151a66e28c88946b74b5
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 00:23:48 -0400

    Import ProcessSteps component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { DashboardPreview } from '@/components/home/dashboard-preview'
 import { StatsBanner } from '@/components/home/stats-banner'
 import { WhyTruckEase } from '@/components/home/why-truckease'
 import { HowWeHelp } from '@/components/home/value-sections'
+import { ProcessSteps } from '@/components/home/process-steps'
 
 export default function Home() {
   return (
