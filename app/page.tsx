bd2f363fccf60d7056fc64cc640932b5b24fbcf9
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 03:44:29 -0400

    Refactor home page components and remove unused sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,33 @@
-import { Hero } from '@/components/home/hero'
-import { DashboardPreview } from '@/components/home/dashboard-preview'
-import { StatsBanner } from '@/components/home/stats-banner'
-import { WhyTruckEase } from '@/components/home/why-truckease'
-import { HowWeHelp } from '@/components/home/value-sections'
-import { ProcessSteps } from '@/components/home/process-steps'
-import { TestimonialsSection } from '@/components/home/testimonials'
-import { RiskCta } from '@/components/home/risk-cta'
+import React from 'react'
+import { Hero } from '@/components/Hero'
+import { DashboardPreview } from '@/components/DashboardPreview'
+import { Features } from '@/components/Features'
+import { ProcessTimeline } from '@/components/ProcessTimeline'
+import { Testimonials } from '@/components/Testimonials'
+import { CallToAction } from '@/components/CallToAction'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-background">
+      {/* 1. Hero Section */}
       <Hero />
+
+      {/* 2. Dashboard Preview Section */}
       <DashboardPreview />
-      <StatsBanner />
-      <WhyTruckEase />
-      <HowWeHelp />
-      <ProcessSteps />
-      <TestimonialsSection />
-      <RiskCta />
+
+      {/* Stats Banner Section Removed Completely */}
+
+      {/* 3. Feature Icons & Services */}
+      <Features />
+
+      {/* 4. Process Timeline */}
+      <ProcessTimeline />
+
+      {/* 5. Client Reviews */}
+      <Testimonials />
+
+      {/* 6. Call To Action Banner */}
+      <CallToAction />
     </main>
   )
 }
