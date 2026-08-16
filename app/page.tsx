85d6ace152477aa473dc39bcb9947f61d94e8c68
mehbobhq <onboard@truckeasesolutions.com>
2026-08-16 00:25:04 -0400

    Add Testimonials component import to page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { StatsBanner } from '@/components/home/stats-banner'
 import { WhyTruckEase } from '@/components/home/why-truckease'
 import { HowWeHelp } from '@/components/home/value-sections'
 import { ProcessSteps } from '@/components/home/process-steps'
+import { Testimonials } from '@/components/home/testimonials'
 
 export default function Home() {
   return (
